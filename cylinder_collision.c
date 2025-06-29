c6e833facef912f28c917f19a67f98d06250869e
TiloVonDerEsche <tilo.voneschwege@gmail.com>
2025-06-29 22:01:50 +0200

    Seemingly working collision, All are left except for one pair RIGHT-(0,0,0)

--- cylinder_collision.c
+++ cylinder_collision.c
@@ -4,39 +4,43 @@
 
 //fills the collision matricies M inside of the track structs
 void track_collision(size_t track_list_len, track** track_list) {
+  if (track_list_len < 2) {puts("LESS THAN TWO TRACKS IN TRACK LIST!");return;}
   //vec3D back_of_machine_vec = {0,-1,0};
   //vec3D sky_vector = {0,0,1};
 
-  track t1; track t2;
+  track* t1; track* t2;
   float distance_t1_t2;
 
   vec3D tv;
 
   for (size_t i = 0; i < track_list_len; i++) {
         //pairwise track extraction
-        t1 = (*track_list)[i];
-        t2 = (*track_list)[i+1];
+        t1 = &(*track_list)[i];
+        t2 = &(*track_list)[i+1];
 
 
 
         //assuming that the tracks are parallel -> point line distance
-        distance_t1_t2 = distance_point_to_line(t1.A,t2.A,connecting_vec(t2.A,t2.B));
+        distance_t1_t2 = distance_point_to_line(t1->A,t2->A,connecting_vec(t2->A,t2->B));
 
-        tv = connecting_vec(t1.A,t2.A);
-        if ((t1.hradius + t2.hradius) >= distance_t1_t2) {
+        tv = connecting_vec(t1->A,t2->A);
+        if ((t1->hradius + t2->hradius) >= distance_t1_t2) {
           //Maybe we should get the Lotfußpunkt and compare t1.A with the Lotfußpunkt
           //on t2
-          
-          if (tv.x > 0) {
-            t2.M.left = 1;
+
+          if (tv.y > 0) {
+            puts("LEFT");
+            t2->M.left = 1;
           }
 
-          else if (tv.x < 0) {
-            t2.M.right = 1;
+          else if (tv.y < 0) {
+            puts("RIGHT");
+            t2->M.right = 1;
           }
           else {
-            t2.M.left = -1;
-            t2.M.right = -1;
+            puts("INSIDE!");
+            t2->M.left = -1;
+            t2->M.right = -1;
           }
 
 

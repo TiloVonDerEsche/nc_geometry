a9fdf05b372b6cb21d0e6d47fc4fd68c6df57547
Tilo von Eschwege <tilo.voneschwege@gmail.com>
2025-08-17 19:20:42 +0200

    Changed path of compile scripts, so that they must be executed in the root of the proj folder

--- src/crosscompile_to_win.sh
+++ src/crosscompile_to_win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-x86_64-w64-mingw32-gcc main.c -o ./../generate_track_list -lm
+x86_64-w64-mingw32-gcc ./src/main.c -o ./generate_track_list.exe -lm
--- track_visualization/opengl/mult_color/compile_linux.sh
+++ track_visualization/opengl/mult_color/compile_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc t_vis_color.c -o 3d_track_visualizer.bin \
+gcc ./track_visualization/opengl/mult_color/t_vis_color.c -o ./3d_track_visualizer.bin \
 -I/home/tilo/res/freeglut/include \
 -L/home/tilo/res/freeglut/build/lib \
 -I/home/tilo/res/glew/include \

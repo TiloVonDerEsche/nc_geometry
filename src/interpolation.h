20c69d68740f9253303510af8353f607fe76d7c7
TiloVonDerEsche <tilo.voneschwege@gmail.com>
2025-09-08 15:30:09 +0200

    Fixed logic error in lin_intpol fn, now correctly intpoling for every tr

--- src/interpolation.h
+++ src/interpolation.h
@@ -10,29 +10,41 @@ void linear_intpol(size_t tl_len, track** tl, Config* config) {
   printf("Opened %s in write mode...\n",config->intpol_csv);
 
 
-  track t = (*tl)[0];
-  vec3D start = t.SP;
-  vec3D end = t.EP;
+  track t; // = (*tl)[0];
+  vec3D start; // = t.SP;
+  vec3D end; // = t.EP;
 
   //get vector start->end
-  vec3D SE = vec_minus(end,start);
-  float SE_len = vec_len(SE);
+  vec3D SE; // = vec_minus(end,start);
+  float SE_len; // = vec_len(SE);
 
 
   //get step vector
-  vec3D track_dir = norm_vec(SE);
+  vec3D track_dir; // = norm_vec(SE);
 
   //vector with length of step_dis in direction of track (start->end)
-  vec3D step_vec = vec_scale(track_dir, config->step_dis);
+  vec3D step_vec; // = vec_scale(track_dir, config->step_dis);
 
   //linear interpolation point
-  vec3D lip = (vec3D) {0,0,0};
+  vec3D lip; //{0,0,0};
   vec3D P; //vec_add(start,lip);
 
 
 
   for (size_t ti = 0; ti < tl_len; ti++) {
+    //prepare intpol for new track
     t = (*tl)[ti];
+    start = t.SP;
+    end = t.EP;
+
+    SE = vec_minus(end,start);
+    SE_len = vec_len(SE);
+
+    track_dir = norm_vec(SE);
+
+    step_vec = vec_scale(track_dir, config->step_dis);
+
+    lip = (vec3D) {0,0,0};
     printf("Interpolation track %lu...\n", (ti+1));
 
     while (vec_len(lip) < SE_len) {

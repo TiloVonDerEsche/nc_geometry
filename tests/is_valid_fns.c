b8ed089e6f2e7072d4127509af59e2f8b11ec150
TiloVonDerEsche <tilo.voneschwege@gmail.com>
2025-09-19 18:02:54 +0200

    Fixed pointer passing type mismatch in is_valid_fns.c test file

--- tests/is_valid_fns.c
+++ tests/is_valid_fns.c
@@ -1,18 +1,20 @@
 #include "./../src/helper_funcs.h"
 
-void is_valid_literal_test(size_t lit_arr_len, char* lit_arr[], uint8_t* test_results[]) {
+void is_valid_literal_test(size_t lit_arr_len, char* lit_arr[], uint8_t test_results[]) {
   for (size_t i = 0; i < lit_arr_len; i++) {
-    printf("Test %lu %s\n",(i+1) ,is_valid_literal(lit_arr[i]) ? "was a success!" : "failed!");
+    printf("Test %lu:'%s', %s\n",(i+1) , lit_arr[i],
+    is_valid_literal(lit_arr[i]) ? "was a success!" : "failed!");
   }
 }
 
 
 int main () {
-  size_t lit_arr_len = 2;
-  char* lit_arr[] = {"1750", "Nope"};
+  size_t lit_arr_len = 12;
+  char* lit_arr[] = {"1750", "Nope", "R8", "G00", "00",
+                  "3.76", "0.12", ".2", "..", "...", "2.7e13", "10^5"};
   uint8_t test_results[lit_arr_len];
 
-  is_valid_literal_test(lit_arr_len, lit_arr, &test_results);
+  is_valid_literal_test(lit_arr_len, lit_arr, test_results);
 
 
   return 0;

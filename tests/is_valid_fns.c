3bdd89ef13a91e16386f1cded1f2d5e014d5f231
TiloVonDerEsche <tilo.voneschwege@gmail.com>
2025-09-19 18:19:52 +0200

    Wrote universal is_valid fn test with typedef for fn pointer, and fn p as parameter

--- tests/is_valid_fns.c
+++ tests/is_valid_fns.c
@@ -1,11 +1,21 @@
 #include "./../src/helper_funcs.h"
 
-void is_valid_literal_test(size_t lit_arr_len, char* lit_arr[], uint8_t test_results[]) {
-  for (size_t i = 0; i < lit_arr_len; i++) {
-    printf("Test %lu:'%s', %s\n",(i+1) , lit_arr[i],
-    is_valid_literal(lit_arr[i]) ? "was a success!" : "failed!");
+typedef uint8_t (*test_fn)(const char*);
+
+void is_valid_test(test_fn fn, size_t arr_len, char* arr[], uint8_t test_results[]) {
+  for (size_t i = 0; i < arr_len; i++) {
+    printf("Test %lu:'%s', %s\n",(i+1) , arr[i],
+    fn(arr[i]) ? "was a success!" : "failed!");
   }
 }
+
+
+// void is_valid_literal_test(size_t lit_arr_len, char* lit_arr[], uint8_t test_results[]) {
+//   for (size_t i = 0; i < lit_arr_len; i++) {
+//     printf("Test %lu:'%s', %s\n",(i+1) , lit_arr[i],
+//     is_valid_literal(lit_arr[i]) ? "was a success!" : "failed!");
+//   }
+// }
 
 
 int main () {
@@ -14,8 +24,15 @@ int main () {
                   "3.76", "0.12", ".2", "..", "...", "2.7e13", "10^5"};
   uint8_t test_results[lit_arr_len];
 
-  is_valid_literal_test(lit_arr_len, lit_arr, test_results);
+  size_t varn_arr_len = 5;
+  char* varn_arr[] = {"R8", "G00", "VARIABLE", "14", "__"};
+  //uint8_t test_results[varn_arr_len];
 
+
+  //is_valid_literal_test(lit_arr_len, lit_arr, test_results);
+  is_valid_test(is_valid_literal, lit_arr_len, lit_arr, test_results);
+  puts("------------");
+  is_valid_test(is_valid_varname, varn_arr_len, varn_arr, test_results);
 
   return 0;
 }

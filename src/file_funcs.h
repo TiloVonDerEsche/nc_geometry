669e2fd96eac622f8863cd573ac17bcf90fde236
TiloVonDerEsche <tilo.voneschwege@gmail.com>
2025-10-01 17:38:40 +0200

    Added hmhis printing functions

--- src/file_funcs.h
+++ src/file_funcs.h
@@ -46,6 +46,35 @@ void print_hashmap(strfloat_t* h) {
   puts("-----");
 }
 
+//write keys at the end of nc program to first line of hmhis.csv
+void print_key_to_hmhis(strfloat_t* h) {
+  printf("Opening: hmhis.csv in write mode...\n");
+  FILE* hmhis = fopen("./data/hmhis.csv", "w");
+  if (hmhis == NULL) {
+      fprintf(stderr, "Error: Could not open hmhis.csv (in write mode)!\n");
+      return;}
+
+
+}
+
+//print one line of hmhis, which is fnums seperated by ','
+void print_values_to_hmhis(strfloat_t* h) {
+  printf("Opening: hmhis.csv in write mode...\n");
+  FILE* hmhis = fopen("./data/hmhis.csv", "w");
+  if (hmhis == NULL) {
+      fprintf(stderr, "Error: Could not open hmhis.csv (in write mode)!\n");
+      return;
+  }
+
+  //Print all variables from hashmap
+  khint_t k;
+  kh_foreach(h, k) {
+      if (k < kh_end(h)) {  // Check if found (k != end iterator)
+          fprintf(hmhis,"%f,", kh_val(h, k));
+      }
+  }
+}
+
 size_t count_lines(FILE* fp) {
   char c;
   size_t lines = 0;

846728c9c56efb0eff7f2f74be2bee610dfcae82
Tilo von Eschwege <tilo.voneschwege@gmail.com>
2026-02-16 13:16:51 +0100

    Cleaned up track_vis duplicate files, and one_color opengl folder

--- track_visualization/opengl/mult_color/compile_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gcc ./track_visualization/opengl/mult_color/t_vis_color.c -o ./3d_track_visualizer.bin \
--I/home/tilo/res/freeglut/include \
--L/home/tilo/res/freeglut/build/lib \
--I/home/tilo/res/glew/include \
--L/home/tilo/res/glew/lib \
--lglut -lGLEW -lGL -lGLU -lm
